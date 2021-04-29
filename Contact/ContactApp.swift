8e724c1314152c825c9159f9931270292da01cae
Cdric_art <cdric.art@gmail.com>
2021-04-29 11:21:29 +0200

    Initial Commit

---
+++ Contact/ContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactApp.swift
+//  Contact
+//
+//  Created by Cédric Evrard on 29/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
